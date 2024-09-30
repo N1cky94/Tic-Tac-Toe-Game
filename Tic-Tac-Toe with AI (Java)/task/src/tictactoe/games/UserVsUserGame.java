05e100815de4112764607389f0d5676ea9c54bf8
Nick Bauters (7dbb) <nick.bauters@colruytgroup.com>
2024-09-30 13:15:42 +0200

    feat: Implement User vs User mode

--- Tic-Tac-Toe with AI (Java)/task/src/tictactoe/games/UserVsUserGame.java
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/games/UserVsUserGame.java
@@ -1,8 +1,21 @@
 package tictactoe.games;
 
 public class UserVsUserGame {
+    private final Game game;
+
+    public UserVsUserGame() {
+        this.game = new Game();
+    }
 
     public void run() {
+        while(game.isNotFinished()) {
+            this.nextTurn();
+        }
 
+        game.printGameCondition();
+    }
+
+    public void nextTurn() {
+        game.nextUserTurn();
     }
 }
