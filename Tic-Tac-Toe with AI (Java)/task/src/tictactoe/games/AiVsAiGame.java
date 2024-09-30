ebad6ee56cb2cdaec162f6d4f7d5e78027d57a48
Nick Bauters (7dbb) <nick.bauters@colruytgroup.com>
2024-09-30 13:12:18 +0200

    feat: Implement AI vs AI mode

--- Tic-Tac-Toe with AI (Java)/task/src/tictactoe/games/AiVsAiGame.java
+++ Tic-Tac-Toe with AI (Java)/task/src/tictactoe/games/AiVsAiGame.java
@@ -1,9 +1,21 @@
 package tictactoe.games;
 
 public class AiVsAiGame {
+    private final Game game;
 
+    public AiVsAiGame() {
+        this.game = new Game();
+    }
 
     public void run() {
+        while(game.isNotFinished()) {
+            this.nextTurn();
+        }
 
+        game.printGameCondition();
+    }
+
+    public void nextTurn() {
+        game.nextAiTurn();
     }
 }
